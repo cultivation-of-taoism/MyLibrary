867224e69c3d42b30780f8ef1f2272d1750567d0
hk <2364121253@qq.com>
2018-08-27 16:24:24 +0800

    解决webview的延时加载问题

--- library/src/main/java/com/hk/library/common/WebViewFragment.kt
+++ library/src/main/java/com/hk/library/common/WebViewFragment.kt
@@ -1,15 +1,20 @@
 package com.hk.library.common
 
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
 import android.widget.FrameLayout
 import com.blankj.utilcode.util.LogUtils
 import com.hk.library.R
+import com.just.agentweb.AbsAgentWebSettings
 import com.just.agentweb.AgentWeb
+import com.just.agentweb.IAgentWebSettings
 import kotlinx.android.synthetic.main.fragment_web_view.*
 
 
@@ -17,9 +22,14 @@ import kotlinx.android.synthetic.main.fragment_web_view.*
  * A simple [Fragment] subclass.
  *
  */
-class WebViewFragment : Fragment() {
+class WebViewFragment : LazyLoadFragment() {
+    override fun setContentView(): Int {
+        return R.layout.fragment_web_view
+    }
+
     lateinit var url: String
     lateinit var agentWeb: AgentWeb
+    lateinit var preWeb: AgentWeb.PreAgentWeb
     companion object {
         fun newInstance(url: String): WebViewFragment {
             val webViewFragment = WebViewFragment()
@@ -34,22 +44,36 @@ class WebViewFragment : Fragment() {
         super.onCreate(savedInstanceState)
         url = arguments.getString(WebViewActivity.EXTRA_URL)
     }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         LogUtils.v(url)
-        agentWeb = AgentWeb.with(this)
+        preWeb = AgentWeb.with(this)
                 .setAgentWebParent(webView_container, FrameLayout.LayoutParams(FrameLayout
                         .LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
                 .useDefaultIndicator(resources.getColor(R.color.colorAccent), 1)
+                .setAgentWebWebSettings(object : AbsAgentWebSettings(){
+                    lateinit var agentWeb: AgentWeb
+                    override fun bindAgentWebSupport(agentWeb: AgentWeb) {
+                        this.agentWeb = agentWeb
+                    }
+
+                    override fun getWebSettings(): WebSettings {
+                        return super.getWebSettings().apply {
+                            //android8.0以上需要关闭安全浏览
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                LogUtils.v(safeBrowsingEnabled)
+                                safeBrowsingEnabled = false
+                            }
+                        }
+                    }
+                })
                 .createAgentWeb()
                 .ready()
-                .go(url)
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun lazyLoad() {
+        agentWeb = preWeb.go(url)
     }
 
     override fun onDestroyView() {
